dce732c25def81e3bdecb8c5b5129e1ed9e7cf79
Sourjya-Sarkar <138858865+Sourjya-Sarkar@users.noreply.github.com>
2026-03-10 01:26:53 +0530

    fix:gitignore-update

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+import type * as server from 'next/server';
+
+export function middleware(request: server.NextRequest) {
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+};
